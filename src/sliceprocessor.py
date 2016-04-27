f180e1c51a127f69936450e57f85ce006b9a83c8
Alex Nelson <alexander.nelson@nist.gov>
2016-04-27 11:38:34 -0400

    Run sequences in order for sense of progress
    Reviewed-by: Lee Badger <mark.badger@nist.gov>
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

--- src/sliceprocessor.py
+++ src/sliceprocessor.py
@@ -1,4 +1,17 @@
-#!/opt/local/bin/python2.7
+#!/usr/bin/env python
+
+# For changes made after April 1, 2016:
+#
+# This software was developed at the National Institute of Standards
+# and Technology by employees of the Federal Government in the course
+# of their official duties. Pursuant to title 17 Section 105 of the
+# United States Code this software is not subject to copyright
+# protection and is in the public domain. NIST assumes no
+# responsibility whatsoever for its use by other parties, and makes
+# no guarantees, expressed or implied, about its quality,
+# reliability, or any other characteristic.
+#
+# We would appreciate acknowledgement if the software is used.
 
 """
 This script:
@@ -7,7 +20,7 @@ This script:
  * Passes each sequence ID (label) to stdout.
 """
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 import os
 import sys
@@ -31,6 +44,8 @@ SELECT DISTINCT
   sequencelabel
 FROM
   diskprint.namedsequence
+ORDER BY
+  sequencelabel
 ;"""
     incursor.execute(query)
     _logger.info("Diskprint table Query: %s" % query)
